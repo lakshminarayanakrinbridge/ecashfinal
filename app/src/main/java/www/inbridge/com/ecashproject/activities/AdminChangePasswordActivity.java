4f682239ce147bb975e7d70d14b55363fd842cc9
Lakshminarayana K R <lakshminarayana.inbridge@gmail.com>
2017-03-23 10:47:57 +0530

    This is a merchantregistration module commit on 23/3/2017

--- app/src/main/java/www/inbridge/com/ecashproject/activities/AdminChangePasswordActivity.java
+++ app/src/main/java/www/inbridge/com/ecashproject/activities/AdminChangePasswordActivity.java
@@ -1,7 +1,9 @@
 package www.inbridge.com.ecashproject.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import www.inbridge.com.ecashproject.R;
 
@@ -11,11 +13,26 @@ public class AdminChangePasswordActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_change_password);
+        Button cancelButton=(Button) findViewById(R.id.cancel_button);
+        Button submitButton=(Button)findViewById(R.id.submit_button);
+        cancelButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
 
-    public void onClickCancelButton() {
-        onBackPressed();
-    }
+
 }
 
