30ab20476206ebb0df59d76e2e5e2438fca51de8
Lakshminarayana K R <lakshminarayana.inbridge@gmail.com>
2017-03-31 12:29:43 +0530

    This is a merchantregistration module commit on 31/3/2017

---
+++ app/src/main/java/www/inbridge/com/ecashproject/activities/MerchantAddOffer.java
@@ -0,0 +1,15 @@
+package www.inbridge.com.ecashproject.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import www.inbridge.com.ecashproject.R;
+
+public class MerchantAddOffer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_merchant_add_offer);
+    }
+}
