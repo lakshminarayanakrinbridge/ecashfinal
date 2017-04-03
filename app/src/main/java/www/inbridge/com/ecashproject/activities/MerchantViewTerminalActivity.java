be9845d3bc7a79747f7425c16dda0dc0aba066e5
Lakshminarayana K R <lakshminarayana.inbridge@gmail.com>
2017-04-03 16:49:18 +0530

    This is a merchantregistration module commit on 4/4/2017

--- app/src/main/java/www/inbridge/com/ecashproject/activities/MerchantViewTerminalActivity.java
+++ app/src/main/java/www/inbridge/com/ecashproject/activities/MerchantViewTerminalActivity.java
@@ -1,16 +1,121 @@
 package www.inbridge.com.ecashproject.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import www.inbridge.com.ecashproject.R;
+import www.inbridge.com.ecashproject.adapters.MerchantViewTerminalAdapter;
+import www.inbridge.com.ecashproject.preferences.Sharedpref;
+import www.inbridge.com.ecashproject.services.Url;
+import www.inbridge.com.ecashproject.utils.MerchantViewTerminalData;
 
 public class MerchantViewTerminalActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter viewMerchantterminalAdapter;
+    List<MerchantViewTerminalData> information = new ArrayList<>();
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_merchant_view_terminal);
         setTitle("Merchant Terminal");
+        recyclerView=(RecyclerView) findViewById(R.id.search_recyclerview1);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, Url.ADMIN_MERCHANT_OFFER_LIST,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        //If we are getting success from server
+                        Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG).show();
+
+                        JSONArray jsonArray;
+                        JSONObject jsonObject;
+                        try {
+                            jsonObject = new JSONObject(response);
+                            jsonArray = jsonObject.getJSONArray("tlist");
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject terminal = jsonArray.getJSONObject(i);
+                                MerchantViewTerminalData adminData = new MerchantViewTerminalData(terminal.getString("tname"), terminal.getString("tnum"), terminal.getString( "temail"),terminal.getString(  "tcode"));
+                                information.add(adminData);
+                                Log.e("value", Integer.toString(information.size()));
+
+                            }
+                            viewMerchantterminalAdapter=new MerchantViewTerminalAdapter(getApplicationContext(),information);
+                            recyclerView.setAdapter(viewMerchantterminalAdapter);
+                            recyclerView.setLayoutManager(new LinearLayoutManager(MerchantViewTerminalActivity.this));
+
+
+
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //You can handle error here if you want
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+
+                SharedPreferences sharedPreferences=MerchantViewTerminalActivity.this.getSharedPreferences(Sharedpref.SHARED_PREF_NAME,MODE_PRIVATE);
+
+
+                params.put(Sharedpref.MERCHANTID_SHARED_PREF,sharedPreferences.getString(Sharedpref.KEY_MERCHANTID,"m_id"));
+                return params;
+            }
+        };
+
+        //Adding the string request to the queue
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        requestQueue.add(stringRequest);
+        Log.e("value1", Integer.toString(information.size()));
+
+
+
     }
+    public void onClickAddnewterminalButton(View v)
+    {
+        Intent i=new Intent(MerchantViewTerminalActivity.this,CreateTerminalActivity.class);
+        startActivity(i);
+
+
+    }
+
 }
--- app/src/main/java/www/inbridge/com/ecashproject/adapters/MerchantViewTerminalAdapter.java
+++ app/src/main/java/www/inbridge/com/ecashproject/adapters/MerchantViewTerminalAdapter.java
@@ -1,8 +1,81 @@
 package www.inbridge.com.ecashproject.adapters;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import www.inbridge.com.ecashproject.R;
+import www.inbridge.com.ecashproject.utils.MerchantViewTerminalData;
+
 /**
  * Created by USER on 2/25/2017.
  */
 
-public class MerchantViewTerminalAdapter {
+
+    public class MerchantViewTerminalAdapter extends RecyclerView.Adapter<MerchantViewTerminalAdapter.MyHolder> {
+
+
+    private LayoutInflater inflater;
+    List<MerchantViewTerminalData> admindata = new ArrayList<>();
+
+    public MerchantViewTerminalAdapter(Context context, List<MerchantViewTerminalData> admindata) {
+        inflater = LayoutInflater.from(context);
+        this.admindata = admindata;
+    }
+
+
+    @Override
+    public MyHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+
+        View view = inflater.inflate(R.layout.merchantviewoffer, parent, false);
+        MyHolder myholder = new MyHolder(view);
+        return myholder;
+    }
+
+    String strcode;
+
+    @Override
+    public void onBindViewHolder(final MyHolder holder, final int position) {
+
+        MerchantViewTerminalData data = admindata.get(position);
+        // holder.textMerchantname.setText(data.getMname());
+        //  holder.textmerchantkey.setText(data.getMkey());
+        holder.cashiername.setText(data.getCashiername());
+        holder.mobilenumber.setText(data.getMobilenumber());
+        holder.emailid.setText(data.getEmailid());
+        holder.terminalid.setText(data.getTerminalid());
+
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return this.admindata.size();
+    }
+
+    class MyHolder extends RecyclerView.ViewHolder {
+        TextView cashiername;
+        TextView mobilenumber;
+        TextView emailid;
+        TextView terminalid;
+
+        public MyHolder(View itemView) {
+            super(itemView);
+            // textMerchantname = (TextView) itemView.findViewById(R.id.tv_mname);
+            terminalid = (TextView) itemView.findViewById(R.id.terminalid_textview);
+            cashiername = (TextView) itemView.findViewById(R.id.cashiername_textview);
+            mobilenumber = (TextView) itemView.findViewById(R.id.mobilenumber_textview);
+            emailid = (TextView) itemView.findViewById(R.id.emailid_textview);
+
+        }
+    }
 }
+
